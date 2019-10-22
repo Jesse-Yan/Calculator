54b3df7661a71b327fa2c62f3af223fb2bf035c4
Outlaw <54597560+JIangZeMingKuangTu@users.noreply.github.com>
2019-10-22 00:45:33 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,39 +1,6 @@
 import java.util.ArrayList;
 
-//import java.util.Stack;
-////Without "(" & ")"
-//public class Calculator {
-//  public static Double evaluate(String expression) {
-//    String[] parts = expression.split(" ");
-//    Stack<Double> s = new Stack<>();
-//    
-//    for (int i=0; i<parts.length; i+=2) {
-//      if (i == 0 || parts[i-1].equals("+")) {
-//        s.push(Double.valueOf(parts[i]));
-//      } else if (parts[i-1].equals("-")) {
-//        s.push(-Double.valueOf(parts[i]));
-//      } else if (parts[i-1].equals("*")) {
-//        s.push(s.pop()*Double.valueOf(parts[i]));
-//      } else if (parts[i-1].equals("/")) {
-//        s.push(s.pop()/Double.valueOf(parts[i]));
-//      }
-//    }
-//    
-//    double r = 0.0;
-//    while(!s.isEmpty()) {
-//      r += s.pop();
-//    }
-//    // your code here
-//    return r;
-//  }
-//}
-
-
 public class Calculator {
-
-  public static void main(String[] args) {
-
-  }
 
   public static Double evaluate(String expression) {
     String[] spliter = expression.split(" ");
